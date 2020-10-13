e13272f7111e44b6863d40076141068af30b3d1a
Sayefdeen Alrawad <sayefdeen.alrawad@gmail.com>
2020-10-13 15:34:20 +0300

    finish

--- index.js
+++ index.js
@@ -5,16 +5,10 @@ const Notes = require('./lib/notes');
 
 const createdObj = new Input();
 const notesObject = new Notes();
-createdObj.valid() ? notesObject.execute(createdObj) : errorMessage(createdObj);
-
-function errorMessage(obj) {
-  console.log(`
-        Entered action was ${obj.action}
-        Entered message should be a string only and not empty
-        Ideal calling is like this :
-        --add "Enter you message here" 
-    `);
-}
+notesObject.execute(createdObj);
 
 // I have worked on this exapmle
 // --add "This is a really cool thing that I wanted to remember for later"
+// --add "this is fun" --category school
+// --list
+// --delete <id for the note>
