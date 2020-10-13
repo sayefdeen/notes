ba01e39dd7ecb499b8431eeeaaf0e5e806277019
Sayefdeen Alrawad <sayefdeen.alrawad@gmail.com>
2020-10-13 15:48:04 +0300

    fix some erros

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const Notes = require('./lib/notes');
 
 const createdObj = new Input();
 const notesObject = new Notes();
-notesObject.execute(createdObj);
+
+if (/a|add|delete|list/.test(createdObj.action)) {
+  notesObject.execute(createdObj);
+} else {
+  notesObject.errorMessage(createdObj);
+}
 
 // I have worked on this exapmle
 // --add "This is a really cool thing that I wanted to remember for later"
